dc59ca060a359b08aa6158884b3a7c20c2bb5682
BlackKnight683 <75422863+Aayush-683@users.noreply.github.com>
2024-02-28 14:18:53 +0530

    email verification pt 1

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const config = require('./config.json');
 const port = config.port;
 const { QuickDB } = require('quick.db');
 const db = new QuickDB({ filePath: './database.sqlite' });
+const nodemailer = require('nodemailer');
+
+// Email Setup
+let transporter2 = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: config.emailUser,
+        pass: config.emailPass
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
 
 // Express Setup
 app.use(express.static('public'));
@@ -20,23 +35,75 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Define Routes
-app.get('/', (req, res) => {
-    if (!req.session.loggedIn) {
-        return res.redirect('/auth');
+// Middleware
+app.use((req, res, next) => {
+    if (req.session.loggedIn) {
+        res.locals.name = req.session.name;
+        res.locals.loggedIn = req.session.loggedIn;
     }
+    next();
+});
+
+// Home Page Route
+app.get('/', (req, res) => {
     res.render('index', { name: req.session.name, loggedIn: req.session.loggedIn });
 });
 
+// About Us Page Route
 app.get('/about', (req, res) => {
     res.render('aboutus');
 })
 
+// Authentication Page Route
 app.get('/auth', (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
     res.render('login-signup', { error: false });
 });
 
+// Email Verification Page Route
+app.get('/verify', (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
+    res.render('verify', { error: false });
+});
+
+// Email Verification Post Route
+app.post('/auth/verify', async (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
+    let email = req.body.email;
+    // Convert Email to Lowercase and replace all dots with underscores
+    email = email.toLowerCase();
+    email = email.replace(/\./g, '_');
+    // Check if Email Exists
+    let emailChk = await db.get(`accounts.${email}`);
+    if (!emailChk) {
+        return res.render('verify', { error: "Email does not exist! Please Signup Instead" });
+    }
+    // Check if Email is already verified
+    let emailVerified = await db.get(`accounts.${email}.verified`);
+    if (emailVerified) {
+        return res.render('verify', { error: "Email is already verified!" });
+    }
+    // Generate Random Code
+    let code = await generateCode();
+    // Save Code
+    await db.set(`verification.${code}`, email);
+    // Send Verification Email
+    await sendVerificationEmail(email, code);
+    res.render('verify', { error: "Verification Email Sent!" });
+});
+
+// Login Post Route
 app.post('/auth/login', async (req, res) => {
+    // Check if user is already logged in
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
     let email = req.body.email;
     let password = req.body.pass;
     // Convert Email to Lowercase and replace all dots with underscores
@@ -61,7 +128,11 @@ app.post('/auth/login', async (req, res) => {
     res.redirect('/');
 });
 
+// Signup Post Route
 app.post('/auth/signup', async (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/');
+    }
     let email = req.body.email;
     let fullName = req.body.name;
     let password = req.body.pass;
@@ -79,12 +150,24 @@ app.post('/auth/signup', async (req, res) => {
     }
     // Encrypt Password
     password = Buffer.from(password).toString('base64');
+    // Verification Code
+    let code = await generateCode();
+    // Save Verification Code
+    await db.set(`verification.${code}`, email);
+    // Send Verification Email
+    await sendVerificationEmail(email, code);
+    // Set Verified to False
+    let verified = false;
     // Save User Data
-    await db.set(`accounts.${email}`, { fullName, password });
+    await db.set(`accounts.${email}`, { fullName, password, verified });
     res.render('login-signup', { error: "Account Created Successfully! Please Login" });
 });
 
+// Logout Route
 app.get('/auth/logout', (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect('/');
+    }
     req.session.destroy();
     res.redirect('/');
 });
@@ -99,3 +182,64 @@ app.listen(port, () => {
     let host = config.domain;
     console.log(`Server started on ${host}:${port}`);
 });
+
+// Email Verification Function
+async function sendVerificationEmail(email, code) {
+    let mailOptions = {
+        from: config.emailSender,
+        to: email,
+        subject: "Email Verification",
+        // HTML Body with css
+        html: `
+        <style>
+            .container {
+                width: 100%;
+                text-align: center;
+            }
+            .button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+            }
+            .code {
+                font-size: 20px;
+            }
+            .codeblock {
+                background-color: #f2f2f2;
+                padding: 10px;
+                border-radius: 5px;
+            }
+        </style>
+        <div class="container">
+            <h1>Email Verification</h1>
+            <p class="code"> Your verification code is: <b class="codeblock">${code}</b></p>
+            <p>Please click the button below to verify your email</p>
+            <a href="${config.domain}/verify" class="button">Verify Email</a>
+        </div>
+        `
+    };
+    await transporter2.sendMail(mailOptions);
+}
+
+// Random unique code generator
+async function generateCode() {
+    // Generate Random Code of 6 Characters
+    let code = Math.random().toString(36).substring(2, 8);
+    // Convert Code to UpperCase
+    code = code.toUpperCase();
+    // Check if Code Exists
+    let check = await db.get(`verification.${code}`);
+    if (check) {
+        let newCode = await generateCode();
+        return newCode;
+    } else {
+        return code;
+    }
+}
